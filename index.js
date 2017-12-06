cba2132b65fe6b3ff94be63477b0e03aea2a2592
d-kang <david.kang714@gmail.com>
2017-12-06 11:43:02 -0800

    Simple Hello World Example

--- index.js
+++
@@ -1,8 +0,0 @@
-/**
-* @Author: David Kang
-* @Date:   12.06.2017 10:44am
-* @Filename: index.js
-* @Last modified time: 12.06.2017 10:44am
-*/
-
-console.log('hello world');
---
+++ src/app.js
@@ -0,0 +1,10 @@
+/**
+* @Author: David Kang
+* @Date:   12.06.2017 10:58am
+* @Filename: app.js
+* @Last modified time: 12.06.2017 11:24am
+*/
+
+
+const hello = 'hello';
+export default hello;
