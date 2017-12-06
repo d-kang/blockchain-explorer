4be0f87956068b1607dfefb4be937c253999822d
d-kang <david.kang714@gmail.com>
2017-12-06 11:59:20 -0800

    Init Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import hello from './app.js';
 console.log('message:', hello);
+
+
+if (module.hot) {
+
+  module.hot.dispose(function () {
+    // module is about to be replaced
+  });
+
+  module.hot.accept(function () {
+    // module or one of its dependencies was just updated
+  });
+}
+
+const $body = document.getElementById('root');
+
+$body.append('hello')
